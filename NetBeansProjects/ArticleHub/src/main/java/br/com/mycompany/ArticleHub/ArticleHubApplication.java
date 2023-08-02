8e75be34927eb6f975eb42e66ba261e98f1d9a98
andesson <andessonreys@gmail.com>
2023-08-02 14:56:41 -0300

    Remover submódulo não desejado

---
+++ NetBeansProjects/ArticleHub/src/main/java/br/com/mycompany/ArticleHub/ArticleHubApplication.java
@@ -0,0 +1,13 @@
+package br.com.mycompany.ArticleHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleHubApplication.class, args);
+	}
+
+}
